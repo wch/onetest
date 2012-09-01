fdce97e49e8cac310ab586e588de6656fb00d8b0
Winston Chang <winston@stdout.org>
2012-09-01 11:21:54 -0500

    Update

--- R/onetest.r
+++ R/onetest.r
@@ -1,9 +1,16 @@
 #' @export
-one_test <- function(){
+one_test <- function() {
   x <- as.POSIXct(Sys.time())
-  cat("new[one_const] ", x[one_const], "\n")
-  cat("new[one_fun()] ", x[one_fun()], "\n")
-  cat("new[one_s3(0)] ", x[one_s3(0)],  "\n")
+  cat("x:", as.character(x), " ", class(x), "\n")
+  cat("x + one_const  ", x + one_const, "\n")
+  cat("x + one_fun()  ", x + one_fun(), "\n")
+  cat("x + one_s3()   ", x + one_s3(), "\n")
+  cat("x[one_const]   ", x[one_const], "\n")
+  cat("x[one_fun()]   ", x[one_fun()], "\n")
+  cat("x[one_s3(0)]   ", x[one_s3(0)],  "\n")
+  cat("x[[one_const]] ", x[[one_const]], "\n")
+  cat("x[[one_fun()]] ", x[[one_fun()]], "\n")
+  cat("x[[one_s3(0)]] ", x[[one_s3(0)]],  "\n")
   invisible()
 }
 
@@ -18,5 +25,3 @@ one_fun <- function()  1
 one_s3 <- function(x)  UseMethod("one_s3")
 #' @S3method one_s3 default
 one_s3.default <- function(x)  1
-
-
